519eba0ac49ab674445a8e939f95466aca0db9be
Jon Baer <jonbaer@gmail.com>
2013-06-06 01:44:10 -0400

    Invalid user-agent?

--- libraries/GithubAPI.rb
+++ libraries/GithubAPI.rb
@@ -16,7 +16,7 @@ module GithubAPI
 		headers = {
 			'Content-Type'  => 'application/x-www-form-urlencoded',
 			'Authorization' =>  "Basic " + Base64.encode64("#{user}:#{pass}").chop,
-			'User-Agent' => 'GithubAPI'
+			'User-Agent' => 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1468.0 Safari/537.36'
 		}
 
 		response = http.post(path, data, headers)
